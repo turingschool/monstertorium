75e45f11bfd1c4c36c12bdbe5b7a220ce2a22479
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-08-15 12:37:26 -0600

    Update api-test.js

--- test/api-test.js
+++ test/api-test.js
@@ -1,3 +1,32 @@
-const assert = require('chai').assert;
-const request = require('supertest');
-const app = require('../server');
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+describe('GET /api/v1/monsters', () => {
+  
+});
+
+describe('POST /api/v1/monsters', () => {
+
+});
+
+describe('UPDATE /monsters/:id', () => {
+
+});
+
+describe('DELETE /monsters/:id', () => {
+
+});
+
+if (!module.parent) { // NEW!
+  app.listen(3000, () => {
+    console.log('The Monstertorium is live! (http://localhost:3000)');
+  });
+}
+
+module.exports = app;
